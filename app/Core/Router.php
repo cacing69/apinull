9557c223843fdb46da3f179400a0d2597cb3af0d
Ibnul Mutaki <ibnuul@gmail.com>
2024-10-18 08:34:53 +0700

    update route

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -7,25 +7,42 @@ use Symfony\Component\Yaml\Yaml;
 class Router
 {
     private $routes;
+    private $allRoutes = [];
 
     public function __construct($configFile)
     {
-        $this->routes = Yaml::parseFile($configFile)['routes'];
+        $this->routes = Yaml::parseFile($configFile);
+
+            if (isset($this->routes['imports'])) {
+                foreach ($this->routes['imports'] as $import) {
+                    $importedRoutes = Yaml::parseFile($import['resource']);
+                    // var_dump($importedRoutes);
+                    // die();
+                    $this->allRoutes = array_merge($this->allRoutes, $importedRoutes['routes']);
+                }
+            }
     }
 
     public function dispatch($requestUri)
     {
-        // var_dump($this->routes);
+        // var_dump($this->allRoutes);
         // die();
-        foreach ($this->routes as $route) {
+        foreach ($this->allRoutes as $route) {
             if ($route['path'] === $requestUri) {
                 // Temukan handler yang sesuai
-                $handlerClass = $route['handler'];
-                $method = $route['method'];
+                // $handlerClass = "Modules\\".$route['handler'];
+                // $method = $route['method'];
+
+            $handlerInfo = explode('::', $route['handler']);
+            $handlerClass = 'Modules\\' . $handlerInfo[0];
+            $handlerMethod = $handlerInfo[1];
+
+                // var_dump($handlerClass);
+                // die();
 
                 if (class_exists($handlerClass)) {
                     $handler = new $handlerClass();
-                    return $handler->$method();
+                    return $handler->$handlerMethod();
                 }
             }
         }
