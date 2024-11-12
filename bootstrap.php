4c0858e0b9367214bf8520b8ab4b065d363d1a51
Ibnul Mutaki <ibnuul@gmail.com>
2024-11-12 12:23:42 +0700

    test vercel

--- bootstrap.php
+++ bootstrap.php
@@ -12,8 +12,8 @@ use Illuminate\Http\Request;
 define("APINULL_PATH", __DIR__);
 
 // Inisialisasi dotenv
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// $dotenv = Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 
 // Baca variabel dari .env
 // $this->rateLimit = getenv('RATE_LIMIT') ?: 100;
