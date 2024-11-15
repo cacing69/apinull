5f20d599f7fd7976d54e443b1d260c8218c183b4
Ibnul Mutaki <ibnuul@gmail.com>
2024-11-15 15:27:44 +0700

    add home roue

---
+++ src/Modules/Home/Http/HomeHandler.php
@@ -0,0 +1,22 @@
+<?php
+namespace Modules\Home\Http;
+
+use App\Http\BaseHandler;
+use Illuminate\Http\Request;
+use Illuminate\Database\Capsule\Manager as DB;
+
+class HomeHandler extends BaseHandler
+{
+    public function index(Request $request)
+    {
+        $data = [
+            "data" => null,
+            "meta" => [
+                "message" => "welcome to ApiNull"
+            ],
+            "error" => null
+        ];
+
+        return $data;
+    }
+}
